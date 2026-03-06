7d30c115706f535e1e5127cbd3a584ef06c9678a
treborfal <77328069+treborfal@users.noreply.github.com>
2026-03-06 16:44:29 +0000

    Clean up legacy page script includes and analytics

--- includes/footer.php
+++ includes/footer.php
@@ -24,14 +24,12 @@
 
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
-<script>window.jQuery || document.write("<script src='/js/vendor/jquery.min.js'><\/script>")</script> 
 <script src="/js/bootstrap.min.js"></script> 
 <script src="/js/script.js"></script> 
 <script src="/js/wow.min.js"></script> 
 <script>
                   new WOW().init();
                   </script> 
-<script src="/js/ie10-viewport-bug-workaround.js"></script> 
 
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -34,9 +34,6 @@
 <!-- Bootstrap core CSS -->
 <link href="/css/bootstrap.min.css" rel="stylesheet">
 
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<link href="/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
 <!-- Custom styles for this template -->
 <link href="/css/animate.css" rel="stylesheet">
 <link href="/css/styles.css" rel="stylesheet">
@@ -63,16 +60,6 @@
     gtag('config', 'G-0FD1QXLLMS'); // Replace with your GA4 Measurement ID
   </script>
 	
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-546243-5', 'auto');
-	  ga('send', 'pageview');
-
-	</script>
   
 	
 	
